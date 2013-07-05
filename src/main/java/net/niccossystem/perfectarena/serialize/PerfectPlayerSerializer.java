061f2ae140c8317f95595f00b7149be07d66100a
NiccosSystem <niccossystem@gmail.com>
2013-07-05 19:04:51 +0200

    Player serialization and deserialization

---
+++ src/main/java/net/niccossystem/perfectarena/serialize/PerfectPlayerSerializer.java
@@ -0,0 +1,37 @@
+package net.niccossystem.perfectarena.serialize;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.bukkit.entity.Player;
+import net.niccossystem.perfectarena.PerfectPlayer;
+
+
+public class PerfectPlayerSerializer {
+    
+    public Map<String, String> serialize(PerfectPlayer player) {
+        Map<String, String> attributes = new HashMap<String, String>();
+        
+        String pClass = player.getPerfectClass().getName();
+        String kills = "Kills:" + String.valueOf(player.getKills());
+        String deaths = "Deaths:" + String.valueOf(player.getDeaths());
+        String pvpDeaths = "PvPKills:" + String.valueOf(player.getPvpDeaths());
+        
+        attributes.put("Class", pClass);
+        attributes.put("Kills", kills);
+        attributes.put("Deaths", deaths);
+        attributes.put("PvPKills", pvpDeaths);
+        
+        return attributes;
+    }
+    
+    public PerfectPlayer deserialize(Player player, Map<String, String> attributes) {
+        PerfectPlayer deserializedPlayer = new PerfectPlayer(player);
+        
+        //deserializedPlayer.setPerfectClass(attributes.get("Class"));
+        deserializedPlayer.setKills(Integer.parseInt(attributes.get("Kills")));
+        deserializedPlayer.setDeaths(Integer.parseInt(attributes.get("Deaths")));
+        deserializedPlayer.setPvpDeaths(Integer.parseInt(attributes.get("PvPKills")));
+        
+        return deserializedPlayer;        
+    }
+}
